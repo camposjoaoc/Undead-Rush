4747ecebe0fc5251a3d1b22ee73c710b2ce6c82c
João Campos <joaocampos963@icloud.com>
2025-09-06 23:57:22 +0200

    chore: Create game project

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] Transform playerModel;
+
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        animator = playerModel.GetComponent<Animator>();
+        spriteRenderer = playerModel.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // old Input system (WASD / arrow keys)
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // movement vector
+        Vector3 movement = new Vector3(x, y, 0f);
+        transform.position += movement.normalized * (moveSpeed * Time.deltaTime);
+
+        // update Idle/Run animations
+        float speed = movement.sqrMagnitude;
+        animator.SetFloat("Speed", speed);
+
+        // flip sprite left/right
+        if (Mathf.Abs(x) > 0.01f)
+        {
+            spriteRenderer.flipX = x < 0;
+        }
+        
+        // test death with K
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Die();
+        }
+
+        // test revive with R
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Revive();
+        }
+    }
+
+    public void Die()
+    {
+        animator.SetBool("IsDead", true); // switch to Dead state
+    }
+
+    public void Revive()
+    {
+        animator.SetBool("IsDead", false); // return to Idle
+    }
+}
