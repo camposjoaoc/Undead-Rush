234120b0f144f18a2df02959e2b9567acaef33f6
João Campos <joaocampos963@icloud.com>
2025-09-09 23:39:52 +0200

    Small changes

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,14 +4,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
     [SerializeField] Transform playerModel;
+    [SerializeField] int maxHealth = 5;
 
-    Animator animator;
-    SpriteRenderer spriteRenderer;
+    private int currentHealth;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     void Awake()
     {
         animator = playerModel.GetComponent<Animator>();
         spriteRenderer = playerModel.GetComponent<SpriteRenderer>();
+        currentHealth = maxHealth;
     }
 
     void Update()
@@ -34,26 +37,32 @@ public class Player : MonoBehaviour
             spriteRenderer.flipX = x < 0;
         }
         
-        // Test death with K
-        if (Input.GetKeyDown(KeyCode.K))
+        // Debug test
+        if (Input.GetKeyDown(KeyCode.K)) TakeDamage(1); // perde 1 de vida
+        if (Input.GetKeyDown(KeyCode.R)) Revive();      // revive
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (currentHealth <= 0) return; // já morto
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
         {
             Die();
-        }
-
-        // Test revive with R
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Revive();
         }
     }
 
     public void Die()
     {
         animator.SetBool("IsDead", true); // switch to Dead state
+        Debug.Log("Player morreu!");
     }
 
     public void Revive()
     {
+        currentHealth = maxHealth;
         animator.SetBool("IsDead", false); // return to Idle
+        Debug.Log("Player reviveu!");
     }
 }
