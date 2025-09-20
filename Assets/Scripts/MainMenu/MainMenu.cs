67e64640f1a915c91aa5ae25472bb8805481c387
João Campos <joaocampos963@icloud.com>
2025-09-20 23:09:09 +0200

    Finish Main menu

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,16 +1,65 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-   
-   public void PlayGame()
+    [Header("UI Panels")] [SerializeField] private GameObject MainMenuUI;
+    [SerializeField] private GameObject SettingsUI;
+
+    [Header("Settings")] [SerializeField] private Slider VolumeSlider;
+    [SerializeField] private Toggle Fullscreen;
+    [SerializeField] private Toggle VSync;
+
+
+    private void Start()
     {
-       SceneManager.LoadScene("GameScene");
+        // Inicializa os controles com os valores salvos
+        VolumeSlider.value = PlayerPrefs.GetFloat("GameVolume", 1f);
+        Fullscreen.isOn = Screen.fullScreen;
+        VSync.isOn = QualitySettings.vSyncCount > 0;
+        
+        ApplyVolume(VolumeSlider.value);
+        
+        // Adiciona listeners aos controles
+        VolumeSlider.onValueChanged.AddListener(ApplyVolume);
+        Fullscreen.onValueChanged.AddListener(SetFullscreen);
+        VSync.onValueChanged.AddListener(SetVSync);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OpenSettings()
+    {
+        MainMenuUI.SetActive(false);
+        SettingsUI.SetActive(true);
+    }
+    public void ShowMainMenu()
+    {
+        SettingsUI.SetActive(false);
+        MainMenuUI.SetActive(true);
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+    
+    //Painel de Configurações
+    public void ApplyVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("GameVolume", volume);
+    }
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void SetVSync(bool isVSync)
+    {
+        QualitySettings.vSyncCount = isVSync ? 1 : 0;
     }
 }
